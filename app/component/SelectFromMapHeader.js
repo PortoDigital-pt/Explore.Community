a9e84bb7077a4072422b3a8387a7b4c95d602775
David Vaz <david.vaz@hyphenteam.com>
2025-02-27 17:52:16 +0000

    refactor select on map to use back button

--- app/component/SelectFromMapHeader.js
+++ app/component/SelectFromMapHeader.js
@@ -1,93 +1,24 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import { string } from 'prop-types';
 import { intlShape } from 'react-intl';
 import { configShape } from '../util/shapes';
-import Icon from './Icon';
+import BackButton from './BackButton';
 
-export default function SelectFromMapHeaderComponent(props, { config, intl }) {
-  const title =
-    props.titleId !== undefined
-      ? intl.formatMessage({
-          id: props.titleId,
-          defaultMessage: 'Select viaPoint'
-        })
-      : '';
-
-  const backBtnCursorPointerClassName = `${
-    props.hideBackBtn ? 'no-pointer' : 'cursor-pointer'
-  }`;
-  const closeBtnCursorPointerClassName = `${
-    props.hideCloseBtn ? 'no-pointer' : 'cursor-pointer'
-  }`;
-
+export default function SelectFromMapHeaderComponent({ titleId }, { intl }) {
   return (
     <div className="select-from-map-nav-container">
-      <button
-        type="button"
-        className={`from-map-modal-nav-button ${backBtnCursorPointerClassName}`}
-        onClick={props.hideBackBtn ? undefined : props.onBackBtnClick}
-        aria-label={intl.formatMessage({
-          id: 'back-button-title',
-          defaultMessage: 'Go back to previous page'
+      <BackButton
+        title={intl.formatMessage({
+          id: titleId,
+          defaultMessage: 'Select viaPoint'
         })}
-      >
-        {!props.hideBackBtn && (
-          <Icon
-            img={props.backBtnIcon}
-            color={config.colors.primary}
-            className={`${props.iconClassName} ${backBtnCursorPointerClassName}`}
-          />
-        )}
-      </button>
-      {title && !props.titleClassName && (
-        <div className="select-from-map-nav-title">{title}</div>
-      )}
-      {title && props.titleClassName && (
-        <span className={props.titleClassName}>{title}</span>
-      )}
-      <button
-        type="button"
-        className={`from-map-modal-nav-button ${closeBtnCursorPointerClassName}`}
-        onClick={props.hideCloseBtn ? undefined : props.onCloseBtnClick}
-        aria-label={intl.formatMessage({
-          id: 'back-button-title',
-          defaultMessage: 'Go back to previous page'
-        })}
-      >
-        {!props.hideCloseBtn && (
-          <Icon
-            img={props.closeBtnIcon}
-            color={config.colors.primary}
-            className={`${props.iconClassName} ${closeBtnCursorPointerClassName}`}
-          />
-        )}
-      </button>
+      />
     </div>
   );
 }
 
 SelectFromMapHeaderComponent.propTypes = {
-  titleId: PropTypes.string,
-  onBackBtnClick: PropTypes.func,
-  onCloseBtnClick: PropTypes.func,
-  backBtnIcon: PropTypes.string,
-  closeBtnIcon: PropTypes.string,
-  hideBackBtn: PropTypes.bool,
-  hideCloseBtn: PropTypes.bool,
-  titleClassName: PropTypes.string,
-  iconClassName: PropTypes.string
-};
-
-SelectFromMapHeaderComponent.defaultProps = {
-  titleId: undefined,
-  backBtnIcon: 'icon-icon_arrow-left',
-  closeBtnIcon: 'icon-icon_close',
-  onBackBtnClick: PropTypes.func,
-  onCloseBtnClick: PropTypes.func,
-  hideBackBtn: false,
-  hideCloseBtn: false,
-  titleClassName: undefined,
-  iconClassName: undefined
+  titleId: string
 };
 
 SelectFromMapHeaderComponent.contextTypes = {
