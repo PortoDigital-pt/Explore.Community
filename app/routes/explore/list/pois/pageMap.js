03fdc85f0ceaf56766da3b493521a80e2296203f
David Vaz <david.vaz@hyphenteam.com>
2025-04-14 16:32:13 +0100

    refactor list page maps

--- app/routes/explore/list/pois/pageMap.js
+++ app/routes/explore/list/pois/pageMap.js
@@ -1,23 +1,6 @@
 import React from 'react';
-import { connectToStores } from 'fluxible-addons-react';
-import { configShape } from '../../../../util/shapes';
-import { mapLayerShape } from '../../../../store/MapLayerStore';
 import ListMap from '../listMap';
 
-const PoiListPageMap = ({ mapLayers }) => <ListMap mapLayers={mapLayers} />;
+const PoiListPageMap = () => <ListMap type="pois" />;
 
-PoiListPageMap.contextTypes = {
-  config: configShape.isRequired
-};
-
-PoiListPageMap.propTypes = {
-  mapLayers: mapLayerShape.isRequired
-};
-
-export default connectToStores(
-  PoiListPageMap,
-  ['MapLayerStore'],
-  ({ getStore }) => ({
-    mapLayers: getStore('MapLayerStore').getFilterLayers({ only: 'pois' })
-  })
-);
+export default PoiListPageMap;
