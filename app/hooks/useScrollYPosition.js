d8b866ba1f47df94ab4721782bac055d1628cf7a
David Vaz <david.vaz@hyphenteam.com>
2025-05-06 11:52:41 +0100

    refactor existent hook to use the scrolling position and created scroll to top button component

--- app/hooks/useScrollYPosition.js
+++ app/hooks/useScrollYPosition.js
@@ -1,29 +1,50 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useLayoutEffect, useState, useMemo, useCallback, useRef } from 'react';
 import debounce from 'lodash/debounce';
 
 const useScrollYPosition = breakpoint => {
   const [scrollY, setScrollY] = useState(0);
+  const firstYPosition = useRef(scrollY);
+  const elementRef = useRef(null);
 
   const elementClassname = useMemo(
     () => (breakpoint === 'large' ? 'scroll-target' : 'drawer-container'),
     [breakpoint]
   );
 
-  useEffect(() => {
-    const handleScroll = debounce(event => {
-      setScrollY(event?.target?.scrollTop ?? 0);
-    }, 200);
-    const element = document.getElementsByClassName(elementClassname)[0];
+  const scrollToTop = useCallback(() => {
+    elementRef.current?.scrollTo({
+      top: firstYPosition.current,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, []);
 
+  const handleScroll = useMemo(
+    () =>
+      debounce(event => {
+        const y = event?.target?.scrollTop ?? 0;
+        setScrollY(y);
+
+        // Store firstYPosition only once for non-large
+        if (breakpoint !== 'large' && firstYPosition.current === 0 && y !== 0) {
+          firstYPosition.current = y;
+        }
+      }, 100),
+    [breakpoint]
+  );
+
+  useLayoutEffect(() => {
+    const element = document.querySelector(`.${elementClassname}`);
+    elementRef.current = element;
     element.addEventListener('scroll', handleScroll, { passive: true });
 
     return () => {
-      element.removeEventListener('scroll', handleScroll);
+      element?.removeEventListener('scroll', handleScroll);
       handleScroll.cancel();
     };
-  }, [elementClassname]);
+  }, [elementClassname, handleScroll]);
 
-  return scrollY;
+  return { scrollY, scrollToTop };
 };
 
 export default useScrollYPosition;
