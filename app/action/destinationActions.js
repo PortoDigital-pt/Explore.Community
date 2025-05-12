33d3b813c2f2f94f6558bc08234e995bc67e32b6
David Vaz <david.vaz@hyphenteam.com>
2025-05-12 14:54:58 +0100

    fix bug introduced in feat/36-profile

--- app/action/destinationActions.js
+++ app/action/destinationActions.js
@@ -1,6 +1,11 @@
 export default function storeDestination(actionContext, destination) {
+  if (Object.keys(destination).length === 0) {
+    actionContext.dispatch('SetDestination', destination);
+    return;
+  }
+
   actionContext.dispatch('SetDestination', {
     ...destination,
-    address: destination.address || destination.name
+    address: destination?.address || destination?.name
   });
 }
