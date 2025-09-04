54023a1c0edc1f2d2927f35907f4d3d861ec5765
David Vaz <david.vaz@hyphenteam.com>
2025-09-04 17:07:15 +0100

    add distance styles

--- app/component/nearyou/StopNearYouHeader.js
+++ app/component/nearyou/StopNearYouHeader.js
@@ -37,7 +37,7 @@ const StopNearYouHeader = ({ stop, desc, isStation, distance }, { config }) => {
             )}
         </div>
       </div>
-      {distance && <div>{distance}</div>}
+      {distance && <div className='distance'>{distance}</div>}
     </div>
   );
 };
